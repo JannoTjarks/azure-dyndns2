6a5e6a397069635de3fe3c7623e7f7f6331a792f
Janno Tjarks <janno.tjarks@mailbox.org>
2025-08-22 01:17:55 +0200

    feat: add version command with central location for the version definition

---
+++ cmd/version.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/JannoTjarks/azure-dyndns2/internal/utils"
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Show version",
+	Long:  "Show the current version of azure-dyndns2",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(utils.Version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import "github.com/JannoTjarks/azure-dyndns2/cmd"
 
-var Version = "0.0.1-dev"
-
 func main() {
 	cmd.Execute()
 }
