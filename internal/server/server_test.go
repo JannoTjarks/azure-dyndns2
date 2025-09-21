e4387157cda86aa4d0aae80e0b351bb43e14c4c0
Janno Tjarks <janno.tjarks@mailbox.org>
2025-09-21 21:40:30 +0200

    test: add more tests for the http handler

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -7,6 +7,21 @@ import (
 	"testing"
 )
 
+func TestNewAzureDynDnsRecord(t *testing.T) {
+	port := "8080"
+	dnsZoneName := "wikipedia.de"
+	resourceGroupName := "publicdns-rg"
+	subscriptionId := "054a27fd-2cd9-42fa-9139-c99cf680fd35"
+
+	want := serverConfig{port, dnsZoneName, resourceGroupName, subscriptionId}
+
+	record := newServerConfig(port, dnsZoneName, resourceGroupName, subscriptionId)
+
+	if record != want {
+		t.Errorf(`utils.serverConfig() = %q, want match for %#q, nil`, record, want)
+	}
+}
+
 func TestVersionHandler(t *testing.T) {
 	req, err := http.NewRequest("GET", "/version", nil)
 	if err != nil {
@@ -42,6 +57,96 @@ func TestVersionHandlerWithExtraPath(t *testing.T) {
 	}
 
 	want := fmt.Sprintf("%s\n", `404 page not found`)
+	if rr.Body.String() != want {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), want)
+	}
+}
+
+func TestRootHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(rootHandler)
+
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	want := fmt.Sprintf("%s\n", "Thanks for using azure-dyndns2!")
+	if rr.Body.String() != want {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), want)
+	}
+}
+
+func TestRootHandlerWrongPath(t *testing.T) {
+	req, err := http.NewRequest("GET", "/test", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(rootHandler)
+
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusNotFound {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusNotFound)
+	}
+}
+
+func TestRootHandlerWierdPath(t *testing.T) {
+	req, err := http.NewRequest("GET", "/../", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(rootHandler)
+
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusNotFound {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusNotFound)
+	}
+}
+
+func TestFallbackHandlerDelete(t *testing.T) {
+	req, err := http.NewRequest("DELETE", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(fallbackHandler)
+
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusMethodNotAllowed {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusMethodNotAllowed)
+	}
+
+	want := fmt.Sprintf("%s\n", "HTTP Method is not allowed!")
+	if rr.Body.String() != want {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), want)
+	}
+}
+
+func TestFallbackHandlerPost(t *testing.T) {
+	req, err := http.NewRequest("POST", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(fallbackHandler)
+
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusMethodNotAllowed {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusMethodNotAllowed)
+	}
+
+	want := fmt.Sprintf("%s\n", "HTTP Method is not allowed!")
 	if rr.Body.String() != want {
 		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), want)
 	}
